baf7cb1a322deabd30ce8958b517b263484bbc14
Michele Caci <michele.caci@amadeus.com>
2018-05-25 13:58:48 +0200

    test refactor

--- Algorithms/src/random/lcg/lcg_serie_index_test.go
+++ Algorithms/src/random/lcg/lcg_serie_index_test.go
@@ -5,26 +5,25 @@ import "testing"
 func TestIndexLCG1(t *testing.T) {
 	testlcg := new(LCG)
 	testlcg.Init(2, 1, 3, 10)
-	t.Logf("Lcg serie index is %f", serieIndex(testlcg))
-	if 1 != serieIndex(testlcg) {
-		t.Fatalf("Lcg serie index is not 1")
-	}
+	indexTest(t, testlcg, 1)
 }
 
 func TestIndexLCG2(t *testing.T) {
 	testlcg := new(LCG)
 	testlcg.Init(1, 1, 4, 6)
-	t.Logf("Lcg serie index is %f", serieIndex(testlcg))
-	if 0.5 != serieIndex(testlcg) {
-		t.Fatalf("Lcg serie index is not 0.5")
-	}
+	indexTest(t, testlcg, 0.5)
 }
 
 func TestIndexLCG3(t *testing.T) {
 	testlcg := new(LCG)
 	testlcg.Init(1, 10, 40, 1000)
-	t.Logf("Lcg serie index is %f", serieIndex(testlcg))
-	if 0 != serieIndex(testlcg) {
-		t.Fatalf("Lcg serie index is not 0")
+	indexTest(t, testlcg, 0)
+}
+
+func indexTest(t *testing.T, lcg *LCG, expectedIndex float64) {
+	index := serieIndex(lcg)
+	t.Logf("Lcg serie index is %f", index)
+	if expectedIndex != index {
+		t.Fatalf("Lcg serie index is not %f", expectedIndex)
 	}
 }
