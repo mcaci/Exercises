e8567a97bd9b9a605ed87913ce81fb8695cedb46
nikiforosFreespirit <michele.caci@gmail.com>
2018-05-28 16:38:13 +0200

    lcg serie small refactor

--- Algorithms/src/random/lcg/lcg_serie_index.go
+++ Algorithms/src/random/lcg/lcg_serie_index.go
@@ -2,14 +2,13 @@ package lcg
 
 func serieIndex(lcg *LCG) float64 {
 	index := 0
-	last := -1
 	current := -1
-	for ; lcg.seed != current; index++ {
+	for last := lcg.seed; lcg.seed != current; index++ {
 		current = lcg.Next()
 		if last == current {
 			return 0.0
 		}
 		last = current
 	}
-	return float64(index)/float64(lcg.m)
+	return float64(index) / float64(lcg.m)
 }
