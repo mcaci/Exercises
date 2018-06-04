702dea4975dfe46d790e8c209a6a9a898cd2976f
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-04 14:12:42 +0200

    second test and code

--- Algorithms/src/numeric/prime/factorization.go
+++ Algorithms/src/numeric/prime/factorization.go
@@ -3,5 +3,5 @@ package prime
 type Primes map[int]int
 
 func Factor(numberToFactor uint) (*Primes, bool) {
-	return new(Primes), true
+	return new(Primes), numberToFactor < 2
 }
--- Algorithms/src/numeric/prime/factorization_test.go
+++ Algorithms/src/numeric/prime/factorization_test.go
@@ -2,11 +2,18 @@ package prime
 
 import "testing"
 
-func TestFactorizationOf0(t *testing.T) {
-	value := 0
-	_, specialCase := Factor(0)
+func TestFactorizationOf0isASpecialCase(t *testing.T) {
+	value := uint(0)
+	_, specialCase := Factor(value)
 	if !specialCase {
-		t.Fatalf("%d is a special case", value)
+		t.Fatalf("%d is a special case of factorization", value)
 	}
 }
 
+func TestFactorizationOf2isNotASpecialCase(t *testing.T) {
+	value := uint(2)
+	_, specialCase := Factor(value)
+	if specialCase {
+		t.Fatalf("%d is not a special case of factorization", value)
+	}
+}
