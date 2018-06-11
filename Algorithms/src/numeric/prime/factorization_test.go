24a434b6adf33d6784ea025a1abf15c04aa70130
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-11 15:30:27 +0200

    Refactor factorization est test

--- Algorithms/src/numeric/prime/factorization_test.go
+++ Algorithms/src/numeric/prime/factorization_test.go
@@ -5,39 +5,23 @@ import (
 )
 
 func TestFactorizationOf0isNil(t *testing.T) {
-	const value uint = 0
-	factors, _ := Factor(value)
-	if len(*factors) > 0 {
-		t.Fatalf("An empty list should be returned as the list of factors and instead %v was returned", factors)
-	}
+	verifyThatFactorsAreExpectedOnes(t, 0, []uint{})
 }
 
 func TestFactorizationOf2is2(t *testing.T) {
-	const value uint = 2
-	factors, _ := Factor(value)
-	var expectedFactors = [1]uint{2}
-	for i, factor := range *factors {
-		if factor != expectedFactors[i] {
-			t.Fatalf("Expected factors are %v but got %v", expectedFactors, *factors)
-		}
-	}
+	verifyThatFactorsAreExpectedOnes(t, 2, []uint{2})
 }
 
 func TestFactorizationOf3is3(t *testing.T) {
-	const value uint = 3
-	factors, _ := Factor(value)
-	var expectedFactors = [1]uint{3}
-	for i, factor := range *factors {
-		if factor != expectedFactors[i] {
-			t.Fatalf("Expected factors are %v but got %v", expectedFactors, *factors)
-		}
-	}
+	verifyThatFactorsAreExpectedOnes(t, 3, []uint{3})
 }
 
 func TestFactorizationOf4is2times2(t *testing.T) {
-	const value uint = 4
+	verifyThatFactorsAreExpectedOnes(t, 4, []uint{2, 2})
+}
+
+func verifyThatFactorsAreExpectedOnes(t *testing.T, value uint, expectedFactors []uint) {
 	factors, _ := Factor(value)
-	var expectedFactors = [2]uint{2, 2}
 	for i, factor := range *factors {
 		if factor != expectedFactors[i] {
 			t.Fatalf("Expected factors are %v but got %v", expectedFactors, *factors)
