54b20b446bccf8f5495c7cc123c430ac1c0d3473
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-11 14:38:30 +0200

    refactoring of tests

---
+++ Algorithms/src/numeric/prime/factorization_specialcase_test.go
@@ -0,0 +1,21 @@
+package prime
+
+import (
+	"testing"
+)
+
+func TestFactorizationOf0isASpecialCase(t *testing.T) {
+	const value uint = 0
+	_, specialCase := Factor(value)
+	if !specialCase {
+		t.Fatalf("%d is a special case of factorization", value)
+	}
+}
+
+func TestFactorizationOf2isNotASpecialCase(t *testing.T) {
+	const value uint = 2
+	_, specialCase := Factor(value)
+	if specialCase {
+		t.Fatalf("%d is not a special case of factorization", value)
+	}
+}
--- Algorithms/src/numeric/prime/factorization_test.go
+++ Algorithms/src/numeric/prime/factorization_test.go
@@ -4,27 +4,11 @@ import (
 	"testing"
 )
 
-func TestFactorizationOf0isASpecialCase(t *testing.T) {
-	const value uint = 0
-	_, specialCase := Factor(value)
-	if !specialCase {
-		t.Fatalf("%d is a special case of factorization", value)
-	}
-}
-
 func TestFactorizationOf0isNil(t *testing.T) {
 	const value uint = 0
 	factors, _ := Factor(value)
 	if len(*factors) > 0 {
 		t.Fatalf("An empty list should be returned as the list of factors and instead %v was returned", factors)
-	}
-}
-
-func TestFactorizationOf2isNotASpecialCase(t *testing.T) {
-	const value uint = 2
-	_, specialCase := Factor(value)
-	if specialCase {
-		t.Fatalf("%d is not a special case of factorization", value)
 	}
 }
 
