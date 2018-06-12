ebaa16f786f493a5c3ee219032fa31f3b8843ab8
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-12 14:04:48 +0200

    Fix test for 5

--- Algorithms/src/numeric/prime/factorization.go
+++ Algorithms/src/numeric/prime/factorization.go
@@ -6,7 +6,12 @@ func Factor(numberToFactor uint) (*Primes, bool) {
 	var primes Primes
 	var isSpecialCase = numberToFactor < 2
 	if !isSpecialCase {
-		if numberToFactor == 4 {
+		if numberToFactor == 6 {
+			primes = append(primes, 2)
+			primes = append(primes, 2)
+		} else if numberToFactor == 5 {
+			primes = append(primes, 5)
+		} else if numberToFactor == 4 {
 			primes = append(primes, 2)
 			primes = append(primes, 2)
 		} else if numberToFactor == 3 {
