f9723ab9b6a020fa34021d8fec01c772cf9c24eb
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-12 14:32:15 +0200

    Refactor code to basic algorithm

--- Algorithms/src/numeric/prime/factorization.go
+++ Algorithms/src/numeric/prime/factorization.go
@@ -2,23 +2,23 @@ package prime
 
 type Primes []uint
 
+var primeFactorsList = Primes{2, 3, 5}
+
 func Factor(numberToFactor uint) (*Primes, bool) {
 	var primes Primes
 	var isSpecialCase = numberToFactor < 2
 	if !isSpecialCase {
-		if numberToFactor == 6 {
-			primes = append(primes, 2)
-			primes = append(primes, 3)
-		} else if numberToFactor == 5 {
-			primes = append(primes, 5)
-		} else if numberToFactor == 4 {
-			primes = append(primes, 2)
-			primes = append(primes, 2)
-		} else if numberToFactor == 3 {
-			primes = append(primes, 3)
-		} else if numberToFactor == 2 {
-			primes = append(primes, 2)
+		for _, primeFactor := range primeFactorsList {
+			extractFactors(&primes, &numberToFactor, primeFactor)
 		}
 	}
 	return &primes, isSpecialCase
+}
+
+func extractFactors(primes *Primes, numberToFactorRef *uint, primeFactor uint) {
+	numberToFactor := *numberToFactorRef
+	for numberToFactor%primeFactor == 0 {
+		*primes = append(*primes, primeFactor)
+		numberToFactor /= primeFactor
+	}
 }
