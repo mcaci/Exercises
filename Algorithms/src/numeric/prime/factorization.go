b50f204e5c592c29a9e07993ce8fd98d5a83c5cc
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-14 19:03:08 +0200

    Refactor code

--- Algorithms/src/numeric/prime/factorization.go
+++ Algorithms/src/numeric/prime/factorization.go
@@ -2,25 +2,46 @@ package prime
 
 type Primes []uint
 
-var primeFactorsList = Primes{2, 3, 5}
-
 func Factor(numberToFactor uint) (*Primes, bool) {
 	var primes Primes
 	var isSpecialCase = numberToFactor < 2
-	var number = numberToFactor
 	if !isSpecialCase {
-		extractFactors(&primes, &numberToFactor, 2)
-		for primeFactor := uint(3); primeFactor <= number; primeFactor += 2 {
-			extractFactors(&primes, &numberToFactor, primeFactor)
-		}
+		var primeFactor uint = 2
+		primes = *extractFactors(&numberToFactor, &primeFactor)
+		primes = append(primes, (*extractLastFactor(&numberToFactor))...)
 	}
 	return &primes, isSpecialCase
 }
 
-func extractFactors(primes *Primes, numberToFactorRef *uint, primeFactor uint) {
-	numberToFactor := *numberToFactorRef
-	for numberToFactor%primeFactor == 0 {
-		*primes = append(*primes, primeFactor)
-		numberToFactor /= primeFactor
+func extractFactors(numberToFactorRef *uint, primeFactorRef *uint) *Primes {
+	var primes = *extractFactor(numberToFactorRef, primeFactorRef)
+	for *primeFactorRef = 3; (*primeFactorRef) * (*primeFactorRef) <= (*numberToFactorRef); (*primeFactorRef) += 2 {
+		primes = append(primes, (*extractFactor(numberToFactorRef, primeFactorRef))...)
 	}
+	return &primes
+}
+
+func extractFactor(numberToFactorRef *uint, primeFactorRef *uint) *Primes {
+	var primes Primes;
+	for (*numberToFactorRef) % (*primeFactorRef) == 0 {
+		primes = append(primes, *addFactor(primeFactorRef)...)
+		updateNumberToFactor(numberToFactorRef, primeFactorRef)
+	}
+	return &primes
+}
+
+func extractLastFactor(primeFactorRef *uint) *Primes {
+	var primes Primes;
+	if (*primeFactorRef) > 1 {
+		primes = *addFactor(primeFactorRef)
+	}
+	return &primes
+}
+
+func addFactor(primeFactorRef *uint) *Primes {
+	return &Primes{*primeFactorRef}
+}
+
+func updateNumberToFactor(numberToFactorRef *uint, primeFactorRef *uint) {
+	(*numberToFactorRef) /= (*primeFactorRef)
 }
