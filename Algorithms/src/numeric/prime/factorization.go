c6a6b7494efb4d6581725444f2f8755f1bacd57e
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-14 16:31:47 +0200

    make test for 7 pass

--- Algorithms/src/numeric/prime/factorization.go
+++ Algorithms/src/numeric/prime/factorization.go
@@ -7,8 +7,10 @@ var primeFactorsList = Primes{2, 3, 5}
 func Factor(numberToFactor uint) (*Primes, bool) {
 	var primes Primes
 	var isSpecialCase = numberToFactor < 2
+	var number = numberToFactor
 	if !isSpecialCase {
-		for _, primeFactor := range primeFactorsList {
+		extractFactors(&primes, &numberToFactor, 2)
+		for primeFactor := uint(3); primeFactor <= number; primeFactor += 2 {
 			extractFactors(&primes, &numberToFactor, primeFactor)
 		}
 	}
