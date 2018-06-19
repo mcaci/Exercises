3a6782aee4f891692b08b8764707d1e2dda38317
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-19 13:12:33 +0200

    Sieve of Eratosthenes implemented

--- Algorithms/src/numeric/primesList/eratosthenes.go
+++ Algorithms/src/numeric/primesList/eratosthenes.go
@@ -1,10 +1,22 @@
 package primesList
 
+
 func ErathosthenesSieve(limit uint) []uint {
-	if limit == 2 {
-		return []uint{2}
-	} else {
-		return []uint{2, 3}
+	isNotPrimeList := make([]bool, limit+1)
+	var number uint
+	for number = 2; number <= limit; number++ {
+		for i := uint(2); number * i < uint(len(isNotPrimeList)); i++ {
+			isNotPrimeList[number * i] = true
+		}
 	}
+	
+	
+	var list []uint
+	for i, isNotPrime := range isNotPrimeList[2:] {
+		if !isNotPrime {
+			list = append(list, uint(i+2))
+		}
+	}
+	return list
 }
 
--- Algorithms/src/numeric/primesList/eratosthenes_test.go
+++ Algorithms/src/numeric/primesList/eratosthenes_test.go
@@ -13,12 +13,16 @@ func TestListOfPrimesUpTo3(t *testing.T) {
 	verify(t, ErathosthenesSieve(3), []uint{2, 3})
 }
 
-// func TestListOfPrimesUpTo4(t *testing.T) {
-// 	verify(t, ErathosthenesSieve(4), []uint{2, 3})
-// }
+func TestListOfPrimesUpTo4(t *testing.T) {
+	verify(t, ErathosthenesSieve(4), []uint{2, 3})
+}
 
 func TestListOfPrimesUpTo5(t *testing.T) {
 	verify(t, ErathosthenesSieve(5), []uint{2, 3, 5})
+}
+
+func TestListOfPrimesUpTo100(t *testing.T) {
+	verify(t, ErathosthenesSieve(100), []uint{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97})
 }
 
 func verify(t *testing.T, list, expectedList []uint) {
