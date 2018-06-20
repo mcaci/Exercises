d07118cce81a1c08410d9c15acbfcb7e896fdf77
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-20 14:48:48 +0200

    Fermat primte test for 2 failing test

---
+++ Algorithms/src/numeric/prime/check/fermat_test.go
@@ -0,0 +1,16 @@
+package check
+
+import "testing"
+
+func TestFermatFor2(t *testing.T) {
+	shouldPrime := true
+	var numberToTest uint = 2
+	isPrime := Fermat(numberToTest)
+	if shouldPrime != isPrime {
+		t.Fatalf("%d is supposed to be prime", numberToTest)
+	}
+}
+
+func Fermat(numberToTest uint) bool {
+	return false
+}
