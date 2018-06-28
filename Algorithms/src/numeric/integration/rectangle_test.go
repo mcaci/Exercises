99769028eab9d99f8f76272211102c3f87312f69
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-28 15:11:44 +0200

    Fix rectangle test

--- Algorithms/src/numeric/integration/rectangle_test.go
+++ Algorithms/src/numeric/integration/rectangle_test.go
@@ -9,7 +9,7 @@ func TestRectangleWithOnePartition(t *testing.T) {
 }
 
 func TestRectangleWithTwoPartitions(t *testing.T) {
-	area := Area(Rectangle, testFunction, 0, 2, 1)
+	area := Area(Rectangle, testFunction, 0, 2, 2)
 	var expectedArea float64 = 3.5
 	verify(t, area, expectedArea)
 }
