303cd48000a5d85b13710e3e3f72c037a490c18a
nikiforosFreespirit <michele.caci@gmail.com>
2018-06-29 14:15:43 +0200

    Refactor ssl prod code

--- Algorithms/src/list/singlelinked/single_linked_list.go
+++ Algorithms/src/list/singlelinked/single_linked_list.go
@@ -11,11 +11,12 @@ func (sll *SingleLinkedList) Add(cell int) {
 }
 
 func (sll *SingleLinkedList) Get(position int) (int, error) {
+	cellValue := 0
+	var err error
 	if sll.counter == 0 {
-		return 0, errors.New("Cannot get from empty list")
+		err = errors.New("Cannot get from empty list")
 	} else if position >= sll.counter {
-		return 0, errors.New("Position requested is out of bounds")
-	} else {
-		return 0, nil
+		err = errors.New("Position requested is out of bounds")
 	}
+	return cellValue, err	
 }
