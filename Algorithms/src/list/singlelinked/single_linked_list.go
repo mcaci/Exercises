e08c281ead6f47fa8f225288d3c240954b3ccff0
nikiforosFreespirit <michele.caci@gmail.com>
2018-07-02 19:34:59 +0200

    Add test and implementation for Single Linked List

--- Algorithms/src/list/singlelinked/single_linked_list.go
+++ Algorithms/src/list/singlelinked/single_linked_list.go
@@ -4,20 +4,42 @@ import "errors"
 
 type SingleLinkedList struct {
 	counter int
+	first *SingleLinkedListCell
+}
+
+type SingleLinkedListCell struct {
 	value int
+	next *SingleLinkedListCell
 }
 
 func (sll *SingleLinkedList) Add(cell int) {
 	sll.counter++;
-	sll.value = cell;
+	if sll.first == nil {
+		sll.first = new(SingleLinkedListCell)
+		sll.first.value = cell
+	} else {
+		node := sll.first
+		for node.next != nil {
+			node = node.next
+		}
+		node.next = new(SingleLinkedListCell)
+		node.next.value = cell
+	}
 }
 
 func (sll *SingleLinkedList) Get(position int) (int, error) {
 	var err error
+	var value int
 	if sll.counter == 0 {
 		err = errors.New("Cannot get from empty list")
 	} else if position >= sll.counter {
 		err = errors.New("Position requested is out of bounds")
+	} else {
+		node := sll.first
+		for i := 0; i < position; i++ {
+			node = node.next
+		}
+		value = node.value
 	}
-	return sll.value, err	
+	return value, err	
 }
--- Algorithms/src/list/singlelinked/single_linked_list_add_test.go
+++ Algorithms/src/list/singlelinked/single_linked_list_add_test.go
@@ -21,3 +21,37 @@ func TestAddDifferentItemIsAddedCorrectly(t *testing.T) {
 		 t.Fatalf("Expected item to be %d but %d was found", addedItem, item)
 	}
 }
+
+func TestAddTwoElementsAndGetFirst(t *testing.T) {
+	var list SingleLinkedList
+	addedItem := 5
+	list.Add(addedItem)
+	list.Add(10)
+	item, _ := list.Get(0)
+	if addedItem != item {
+		 t.Fatalf("Expected item to be %d but %d was found", addedItem, item)
+	}
+}
+
+func TestAddTwoElementsAndGetSecond(t *testing.T) {
+	var list SingleLinkedList
+	addedItem := 10
+	list.Add(5)
+	list.Add(addedItem)
+	item, _ := list.Get(1)
+	if addedItem != item {
+		 t.Fatalf("Expected item to be %d but %d was found", addedItem, item)
+	}
+}
+
+func TestAddThreeElementsAndGetThird(t *testing.T) {
+	var list SingleLinkedList
+	addedItem := 10
+	list.Add(5)
+	list.Add(5)
+	list.Add(addedItem)
+	item, _ := list.Get(2)
+	if addedItem != item {
+		 t.Fatalf("Expected item to be %d but %d was found", addedItem, item)
+	}
+}
