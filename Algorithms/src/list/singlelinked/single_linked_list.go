640f5d5c0c8ea466bf8660f78b300e53fadcb954
nikiforosFreespirit <michele.caci@gmail.com>
2018-07-05 13:17:21 +0200

    Extracting method from single linked list

--- Algorithms/src/list/singlelinked/single_linked_list.go
+++ Algorithms/src/list/singlelinked/single_linked_list.go
@@ -15,31 +15,51 @@ type SingleLinkedListCell struct {
 func (sll *SingleLinkedList) Add(cell int) {
 	sll.counter++;
 	if sll.first == nil {
-		sll.first = new(SingleLinkedListCell)
-		sll.first.value = cell
+		sll.first = createAndAddCell(cell)
 	} else {
-		node := sll.first
-		for node.next != nil {
-			node = node.next
-		}
-		node.next = new(SingleLinkedListCell)
-		node.next.value = cell
+		node := sll.iterateToTheEndOfTheList()
+		node.next = createAndAddCell(cell)
 	}
 }
 
 func (sll *SingleLinkedList) Get(position int) (int, error) {
-	var err error
+	err := errorCheck(sll.counter, position)
 	var value int
-	if sll.counter == 0 {
-		err = errors.New("Cannot get from empty list")
-	} else if position >= sll.counter {
-		err = errors.New("Position requested is out of bounds")
-	} else {
-		node := sll.first
-		for i := 0; i < position; i++ {
-			node = node.next
-		}
+	if err == nil {
+		node := sll.iterateTo(position)
 		value = node.value
 	}
 	return value, err	
+}
+
+func errorCheck(listSize, position int) error {
+	var err error
+	if listSize == 0 {
+		err = errors.New("Cannot get from empty list")
+	} else if position >= listSize {
+		err = errors.New("Position requested is out of bounds")
+	}
+	return err
+}
+
+func createAndAddCell(cell int) *SingleLinkedListCell {
+	sllCell := new(SingleLinkedListCell)
+	sllCell.value = cell
+	return sllCell
+}
+
+func (sll *SingleLinkedList) iterateToTheEndOfTheList() *SingleLinkedListCell {
+	node := sll.first
+	for node.next != nil {
+		node = node.next
+	}
+	return node
+}
+
+func (sll *SingleLinkedList) iterateTo(position int) *SingleLinkedListCell {
+	node := sll.first
+	for i := 0; i < position; i++ {
+		node = node.next
+	}
+	return node
 }
